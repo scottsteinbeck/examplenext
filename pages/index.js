57ed25140677cf5c68e11e0e514a7e1f52f398f4
Scott Steinbeck <uniquetrio2000@gmail.com>
2021-04-08 13:01:16 -0700

    cutting back

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export default createProxyMiddleware({
-    target: 'http://agritrackingsystems.com:3000',
-    changeOrigin: true,
-    prependPath: true,
-    ws: true,
-});
