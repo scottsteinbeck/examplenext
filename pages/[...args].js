ade00c5fa7294d8a9a03df7e99b055385291f300
Scott Steinbeck <uniquetrio2000@gmail.com>
2021-04-08 13:13:42 -0700

    start again with a few changes

---
+++ pages/[...args].js
@@ -0,0 +1,7 @@
+import  {  createProxyMiddleware  }  from  'http-proxy-middleware' ;
+
+export  default  createProxyMiddleware ( {
+    target : 'http://agritrackingsystems.com:3000',
+    changeOrigin : true ,
+    prependPath : true ,
+} ) ;
